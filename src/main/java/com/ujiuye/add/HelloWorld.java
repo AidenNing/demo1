be8538ae15b92c4dea8ff128061b0b842cd643c2
AidenNing <ning888888@yeah.net>
2019-05-20 16:37:24 +0800

    thrid

--- src/main/java/com/ujiuye/add/HelloWorld.java
+++ src/main/java/com/ujiuye/add/HelloWorld.java
@@ -3,5 +3,8 @@ package com.ujiuye.add;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        System.out.println("thrid");
+
+
     }
 }
